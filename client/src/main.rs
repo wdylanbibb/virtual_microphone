971b12522def02c6fc6ab47aee703795276a17ce
Dylan Bibb <wdylanbibb@gmail.com>
2024-02-14 14:19:18 -0500

    Moved listener to client folder

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,39 @@
+use std::io::{self, Read};
+use std::{
+    fs::File,
+    net::{TcpListener, TcpStream},
+    thread,
+};
+
+fn handle_client(mut stream: TcpStream) -> io::Result<File> {
+    let buf: &mut [u8; 13] = &mut [0; 13];
+    loop {
+        let result = stream.read(buf);
+        match result {
+            Ok(len) => {
+                let str = String::from_utf8(buf[0..len].to_vec());
+
+                println!("wrote: {:?}", str);
+            }
+            Err(e) => {
+                println!("error parsing header: {:?}", e);
+                return Err(e);
+            }
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:34234").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    // connection succeeded
+                    handle_client(stream)
+                });
+            }
+            Err(_) => { /* connection failed */ }
+        }
+    }
 }
--- host/src/main.rs
+++ host/src/main.rs
@@ -1,51 +1,13 @@
-use std::fs::File;
-use std::io;
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
-use std::thread;
-use std::time::Duration;
-
-fn handle_client(mut stream: TcpStream) -> io::Result<File> {
-    let buf: &mut [u8; 100] = &mut [0; 100];
-    loop {
-        let result = stream.read(buf);
-        match result {
-            Ok(len) => {
-                let str = String::from_utf8(buf[0..len].to_vec());
-
-                println!("wrote: {:?}", str);
-            }
-            Err(e) => {
-                println!("error parsing header: {:?}", e);
-                return Err(e);
-            }
-        }
-    }
-}
+use std::net::TcpStream;
 
 fn main() {
-    println!("Hello, world!");
-
-    thread::spawn(move || {
-        let listener = TcpListener::bind("127.0.0.1:34234").unwrap();
-
-        for stream in listener.incoming() {
-            match stream {
-                Ok(stream) => {
-                    thread::spawn(move || {
-                        // connection succeeded
-                        handle_client(stream)
-                    });
-                }
-                Err(_) => { /* connection failed */ }
-            }
-        }
-    });
-
     let mut client = TcpStream::connect("127.0.0.1:34234").unwrap();
-    match client.write(b"I'm a teapot!") {
-        Ok(len) => println!("wrote {} bytes", len),
-        Err(e) => println!("error parsing header: {:?}", e),
+    loop {
+        match client.write(b"I'm a teapot!") {
+            Ok(len) => println!("wrote {} bytes", len),
+            Err(e) => println!("error parsing header: {:?}", e),
+        }
+        // thread::sleep(Duration::new(1, 0));
     }
-    thread::sleep(Duration::new(1, 0));
 }
